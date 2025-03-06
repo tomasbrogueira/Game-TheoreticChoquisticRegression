9c8a5f360cbdf9319d21a117bf4ec166575622b9
Tomas Brogueira <tomas.brogueira@tecnico.ulisboa.pt>
2025-03-06 16:38:49 +0000

    presentation 1 done

--- paper improvement/main.py
+++ paper improvement/main.py
@@ -4,8 +4,8 @@ from results_processing import results_processing
 
 # Datasets
 # datasets = ['dados_covid_sbpo_atual','banknotes','transfusion','mammographic','raisin','rice','diabetes','skin']
-#datasets = ["transfusion"]
-datasets = ["mammographic"]
+datasets = ["transfusion"]
+#datasets = ["mammographic"]
 #datasets = ['dados_covid_sbpo_atual']
 
 for dataset in datasets:
